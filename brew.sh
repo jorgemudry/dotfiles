db459ca80fbbb9b294915319d683aa0be9474579
Jorge Mudry <jorgemudry@gmail.com>
2015-10-03 22:34:40 -0300

    Disable some software from brew.sh file

--- brew.sh
+++ brew.sh
@@ -20,20 +20,20 @@ brew tap homebrew/dupes
 
 # Install GNU core utilities (those that come with OS X are outdated).
 # Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+#brew install coreutils
+#sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
 
 # Install some other useful utilities like `sponge`.
-brew install moreutils
+#brew install moreutils
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
+#brew install findutils
 # Install GNU `sed`, overwriting the built-in `sed`.
-brew install gnu-sed --with-default-names
+#brew install gnu-sed --with-default-names
 # Install Bash 4.
 # Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
 # running `chsh`.
-brew install bash
-brew install bash-completion
+#brew install bash
+#brew install bash-completion
 
 # Install `wget` with IRI support.
 brew install wget --with-iri
@@ -50,44 +50,44 @@ brew install homebrew/dupes/grep
 brew install homebrew/dupes/screen
 
 # Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
+# brew install bfg
+# brew install binutils
+# brew install binwalk
+# brew install cifer
+# brew install dex2jar
+# brew install dns2tcp
+# brew install fcrackzip
+# brew install foremost
+# brew install hashpump
+# brew install hydra
+# brew install john
+# brew install knock
+# brew install nmap
+# brew install pngcheck
+# brew install socat
+# brew install sqlmap
+# brew install tcpflow
+# brew install tcpreplay
+# brew install tcptrace
+# brew install ucspi-tcp # `tcpserver` etc.
+# brew install xpdf
+# brew install xz
 
 # Install other useful binaries.
-brew install ack
+# brew install ack
 brew install git
 brew install ssh-copy-id
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
+# brew install imagemagick --with-webp
+# brew install lua
+# brew install lynx
+# brew install p7zip
+# brew install pigz
 brew install pv
 brew install rename
-brew install rhino
+# brew install rhino
 brew install tree
 brew install webkit2png
-brew install zopfli
+# brew install zopfli
 brew install homebrew/php/php55-mcrypt
 brew install mysql
 
@@ -107,14 +107,15 @@ brew cask install firefox
 brew cask install dropbox
 brew cask install google-drive
 brew cask install evernote
-brew cask install skitch
-brew cask install jumpcut
+# brew cask install skitch
+# brew cask install jumpcut
 
 # Messaging
 brew cask install skype
-brew cask install google-hangouts
-brew cask install limechat
+# brew cask install google-hangouts
+# brew cask install limechat
 brew cask install telegram
+brew cask install hipchat
 
 # Media
 brew cask install vlc
@@ -122,8 +123,8 @@ brew cask install spotify
 
 # File Transfer
 brew cask install transmission
-brew cask install utorrent
-brew cask install transmit
+# brew cask install utorrent
+# brew cask install transmit
 brew cask install bittorrent-sync
 
 # Development
@@ -131,6 +132,7 @@ brew cask install sequel-pro
 brew cask install vagrant
 brew cask install sublime-text
 brew cask install virtualbox
+brew cask install paw
 
 # Utilities
 brew cask install iterm2
@@ -140,10 +142,9 @@ brew cask install flux
 brew cask install onepassword
 brew cask install coconutbattery
 brew cask install spectacle
-brew cask install cleanmymac
 brew cask install android-file-transfer
 brew cask install cheatsheet
-brew cask install onyx
+# brew cask install onyx
 brew cask install cronnix
 
 # Remove outdated versions from the cellar.
